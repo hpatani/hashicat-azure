27cc365fca6573b734ead5c7c9e499521a3da32a
hpatani <harshal.patani@gmail.com>
2019-11-11 12:55:02 +0000

    Updated Beardo

--- files/deploy_app.sh
+++ files/deploy_app.sh
@@ -4,13 +4,13 @@
 
 cat << EOM > /var/www/html/index.html
 <html>
-  <head><title>Beards!</title></head>
+  <head><title>Beardos!</title></head>
   <body>
   <div style="width:800px;margin: 0 auto">
 
   <!-- BEGIN -->
   <center><img src="http://${PLACEHOLDER}/${WIDTH}/${HEIGHT}"></img></center>
-  <center><h2>Beard World!</h2></center>
+  <center><h2>Beardo World!</h2></center>
   Welcome to ${PREFIX}'s app. Replace this text with your own. 
   <!-- END -->
   
